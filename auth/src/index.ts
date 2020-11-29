b8f34b0c8531725ff2ed4e358258d6c1b9db2df6
Tanmay Nale <tvnale@gmail.com>
2020-11-29 21:14:49 +0530

    Async error Handling express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -15,7 +17,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*',()=>{
+app.all('*', async ()=>{
     throw new NotFoundError();
 });
 
