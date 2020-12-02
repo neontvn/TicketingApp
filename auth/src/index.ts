2344f88bc095a60614f05356eb0ccffab07b27d0
Tanmay Nale <tvnale@gmail.com>
2020-12-02 13:47:47 +0530

    Connected to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose, { mongo } from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -23,6 +24,22 @@ app.all('*', async ()=>{
 
 app.use(errorHandler);
 
-app.listen(3000,()=>{
-    console.log("Listening on 3000!!!!");
-})
+const start = async() => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log("Connected to MongoDb");
+    }catch(err){
+        console.error(err);
+    }
+
+    app.listen(3000,()=>{
+        console.log("Listening on 3000!!!!");
+    })
+
+}
+
+start();
