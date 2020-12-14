0ee358fd81c17b5c85d43ee566a907b4367d0bdb
Tanmay Nale <tvnale@gmail.com>
2020-12-14 14:18:49 +0530

    Custom request hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,54 +1,46 @@
 import { useState } from "react";
-import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+  });
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-    } catch (err) {
-      console.log(err.response.data.errors);
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          className="form-control"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Oops....</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
+      <div style={{ width : '50%' }}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-      )}
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
