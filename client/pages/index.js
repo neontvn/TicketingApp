3a8bc7a03800c83b7672070986f8d14244b676ee
Tanmay Nale <tvnale@gmail.com>
2020-12-21 23:09:54 +0530

    nats deployment updated

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,7 +8,7 @@ import buildClient from '../api/build-client'; // Reusable API client for server
 
 
 const LandingPage =  ({ currentUser }) => {    
-    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
+    return currentUser ? <h1 >You are signed in</h1> : <h1>You are not signed in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
