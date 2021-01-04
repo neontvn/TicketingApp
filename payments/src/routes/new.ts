8fe6977fc22e3ca607e5fac0e1cd4f4331fd4f29
Tanmay Nale <tvnale@gmail.com>
2021-01-04 11:52:50 +0530

    Payments service completed

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import {
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -58,8 +60,12 @@ router.post('/api/payments',
             stripeId : charge.id
         });
         await payment.save();
-
-        res.status(201).send({ success: true });
+        new PaymentCreatedPublisher(natsWrapper.client).publish({
+            id : payment.id,
+            orderId : payment.orderId,
+            stripeId : payment.stripeId
+        });
+        res.status(201).send({ id: payment.id, success: true });
     });
 
 
